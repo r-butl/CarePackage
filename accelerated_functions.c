22065644c55414b21655d1ab841a4fe48d33460b
Lucas Butler <08lbutler31@gmail.com>
2023-11-17 00:10:12 -0800

    updated users in erd

--- accelerated_functions.c
+++ accelerated_functions.c
@@ -12,39 +12,14 @@ int get_length(int *a){
         return length;
 }
 
-int* low_pass(int* input, int sample_per_sec, int w0)
+int* wavelet(int* input)
 {
-/*
-	INPUT:
-		input 		- integer array	
-		sample_per_sec - Sample frequency of the signal
-		w0 			- Desired corner frequency of the signal
-
-	y(nT) = 2y(nT- T) - y(nT - 2T) + x(nT) - 2x(nT - 6T) + x(nT - 12T)
-
-	T - sampling period 
-
-	
-*/
-
 	return NULL;
+
 }
 
-int* high_pass()
+int* median(int* input)
 {
 
 	return NULL;
 }
-
-int* deriv_filter()
-{
-
-	return NULL;
-}
-
-int* square()
-{
-
-	return NULL;
-}
-
